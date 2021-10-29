8dada0459d302cdb26318e6cda6167030947a79c
GeetaSMath <geetasmath@gmail.com>
2021-10-29 11:48:14 -0600

    Added: AddressBook Problem for Multiple System

---
+++ src/com/bridgelabz/AddressBookMul.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class AddressBookMul {
+    public static void main(String[] args) {
+        System.out.println("Welcome to solve Multiple Records AddressBook");
+    }
+}
