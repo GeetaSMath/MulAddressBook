513f314ce5b2771324565d8f5e55e28005087d10
GeetaSMath <geetasmath@gmail.com>
2021-10-29 12:08:42 -0600

    Added: Add Contact To AddressBook

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,20 +1,58 @@
 package com.bridgelabz;
-
+import java.util.*;
 public class AddressBook {
 
-        public static void main(String[] args) {
-            AddressBookMul b = new AddressBookMul();
-            System.out.println("Welcome to Address Book Program");
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-            b.setFirstName("Afroz");
-            b.setLastName("Basha");
-            b.setCellNum("95xx605");
-            b.setEmail("afrozxx@gmail.com");
-            b.setCity("jammalamadugu");
-            b.setZip("516434");
-            b.setState("Andhra Pradesh");
+        AddressBookMul book1 = new AddressBookMul();
+            ArrayList<String> man1 = new ArrayList<String>();
 
-            System.out.println(" First Name: "+b.getFirstName()+"\n Last Name: "+b.getLastName()+"\n Cell Num: "+b.getCellNum()+"\n Email-id: "+b.getEmail()+"\n City: "+b.getCity()+" \n Pin: "+b.getZip()+"\n State: "+b.getState());
+
+            System.out.println("Add a new Contact to Address Book");
+
+            System.out.print("Enter First Name : ");
+            String fN = sc.nextLine();
+            book1.setFirstName(fN);
+            man1.add(0, book1.getFirstName());
+
+
+            System.out.print("Enter Last Name : ");
+            String lN = sc.nextLine();
+            book1.setLastName(lN);
+            man1.add(1, book1.getLastName());
+
+            System.out.print("Enter Email-id : ");
+            String eId = sc.nextLine();
+            book1.setEmail(eId);
+            man1.add(2, book1.getEmail());
+
+            System.out.print("Enter Cell Num : ");
+            String cell = sc.nextLine();
+            book1.setCellNum(cell);
+            man1.add(3, book1.getCellNum());
+
+            System.out.print("Enter City : ");
+            String city = sc.nextLine();
+            book1.setCity(city);
+            man1.add(4, book1.getCity());
+
+            System.out.print("Enter Zip : ");
+            String zIp = sc.nextLine();
+            book1.setZip(zIp);
+            man1.add(5, book1.getZip());
+
+            System.out.print("Enter Sate : ");
+            String state = sc.nextLine();
+            book1.setState(state);
+            man1.add(6, book1.getState());
+
+            System.out.println(man1);
+
+            System.out.println(" First Name: "+man1.get(0)+"\n Last Name: "+man1.get(1)+"\n Cell Num: "+man1.get(2)+"\n Email-id: "+man1.get(3)+"\n City: "+man1.get(4)+" \n Pin: "+man1.get(5)+"\n State: "+man1.get(6));
+
 
         }
     }
+
+
