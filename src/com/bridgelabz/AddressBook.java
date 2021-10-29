c254e123fa894d479026e8ed3afb5322be0b8db4
GeetaSMath <geetasmath@gmail.com>
2021-10-29 12:02:23 -0600

    Added: Added Contact information

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class AddressBook {
+
+        public static void main(String[] args) {
+            AddressBookMul b = new AddressBookMul();
+            System.out.println("Welcome to Address Book Program");
+
+            b.setFirstName("Afroz");
+            b.setLastName("Basha");
+            b.setCellNum("95xx605");
+            b.setEmail("afrozxx@gmail.com");
+            b.setCity("jammalamadugu");
+            b.setZip("516434");
+            b.setState("Andhra Pradesh");
+
+            System.out.println(" First Name: "+b.getFirstName()+"\n Last Name: "+b.getLastName()+"\n Cell Num: "+b.getCellNum()+"\n Email-id: "+b.getEmail()+"\n City: "+b.getCity()+" \n Pin: "+b.getZip()+"\n State: "+b.getState());
+
+        }
+    }
