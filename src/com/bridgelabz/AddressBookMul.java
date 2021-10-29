23e928d0e2822e039ea01c20957eba49273fdcf9
GeetaSMath <geetasmath@gmail.com>
2021-10-29 11:52:43 -0600

    Added: Created Contact of AddressBook

--- src/com/bridgelabz/AddressBookMul.java
+++ src/com/bridgelabz/AddressBookMul.java
@@ -1,7 +1,56 @@
 package com.bridgelabz;
 
 public class AddressBookMul {
-    public static void main(String[] args) {
-        System.out.println("Welcome to solve Multiple Records AddressBook");
+
+    private String firstName, lastName, city, state, zip, cellNum, email;
+
+    public void setFirstName(String fname) {
+        this.firstName = fname;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lname) {
+        this.lastName = lname;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setCity(String cname) {
+        this.city = cname;
+    }
+    public String getCity() {
+        return city;
+    }
+
+    public void setZip(String pin) {
+        this.zip = pin;
+    }
+    public String getZip() {
+        return zip;
+    }
+
+    public void setCellNum(String cell) {
+        this.cellNum = cell;
+    }
+    public String getCellNum() {
+        return cellNum;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getState() {
+        return state;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
     }
 }
+
